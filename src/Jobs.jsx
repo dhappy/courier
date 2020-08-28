67a63ff8b0896613d5310c78b2ae106c27a44dc3
Will Holcomb <dys@dhappy.org>
2020-08-28 07:16:06 -0400

    addin' 3box - fixes

--- src/Jobs.jsx
+++ src/Jobs.jsx
@@ -1,20 +1,46 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react'
 import { Map as LeafletMap, Marker, Popup, TileLayer, Polygon, FeatureGroup } from 'react-leaflet'
 import { EditControl } from 'react-leaflet-draw'
-import { Flex, Heading } from 'rimble-ui'
+import { Card, Flex, Heading, Table, Form, Input, Button, Loader } from 'rimble-ui'
+import Box from '3box'
 //import 'leaflet/dist/leaflet.css'
 import 'leaflet-draw/dist/leaflet.draw.css'
+import Web3 from 'web3'
 
 export default () => {
   const [pos, setPos] = useState({ y: 51.505, x: -0.09 })
   const [hexs, setHexs] = useState([])
-  const [selected, setSelection] = useState([])
+  const [zones, setZones] = useState([])
+  const [contracts, setContracts] = useState([])
+  const [name, setName] = useState()
+  const web3 = new Web3(Web3.givenProvider)
 
   useEffect(() => {
     const success = (pos) => setPos({ y: pos.coords.latitude, x: pos.coords.longitude })
     const error = (err) => alert(`Error Getting Current Position: '${err.message}'`)
     navigator.geolocation.getCurrentPosition(success, error)
   }, [])
+
+  const getProfile = useCallback(async () => {
+    setName(<Loader size='80px'/>)
+    const user = (await web3.eth.getAccounts())[0]
+    console.info('CB', user)
+    const box = await Box.openBox(user, Web3.givenProvider)
+    console.info('BX', box)
+    await box.syncDone
+    console.info('SD', box)
+    const name = await box.public.get('name')
+    console.info('PFL', name)
+    setName(name)
+  }, [])
+
+  useEffect(() => { getProfile() }, [getProfile])
+
+  const extractContracts = useCallback(async (zones) => {
+    //for(let zone of zones) {}
+  }, [web3.eth])
+
+  useEffect(() => { extractContracts(zones) }, [extractContracts, zones])
 
   useEffect(() => {
     const w = 0.015
@@ -41,7 +67,7 @@ export default () => {
           ),
           y: (
             pos.y + (i % rows) * 3 * w
-            - (full.height / 2 + one.height / 2)
+            - (3 * full.height / 4 + one.height / 2)
           ),
         }
         if(row % 2 === 1) {
@@ -55,19 +81,21 @@ export default () => {
           [p.y + w * Math.cos(theta), p.x - w * Math.sin(theta)],
           [p.y - w * Math.cos(theta), p.x - w * Math.sin(theta)],
         ]
+        poly.center = p
         poly.index = { row: start.row + row, col: start.col + i % cols }
+        poly.contracts = []
         return poly
       })
     )
     setHexs(polys)
 
     const r = w * 10
-    setSelection([...new Array(polys.length)].map((_, i) => (
-      polys[i].every((p) => (
-        // ToDo: Fix this to be a circle
-        Math.sqrt(Math.pow(p[1] - pos.x, 2) - Math.pow(p[0] - pos.y, 2)) > r
+    const zones = polys.filter((poly) => (
+      poly.every((p) => (
+        Math.sqrt(Math.pow(p[1] - pos.x, 2) + Math.pow(p[0] - pos.y, 2)) <= r
       ))
-    )))
+    ))
+    setZones(zones)
   }, [pos])
 
   const onPathCreate = console.info
@@ -81,8 +109,11 @@ export default () => {
 
   return (
     <Flex alignItems='center' flexDirection='column'>
+      {name &&
+        <Card><Heading>{name}</Heading></Card>
+      }
       <Heading>Select Your Availability Zone</Heading>
-      <LeafletMap center={[pos.y, pos.x]} zoom={12}>
+      <LeafletMap center={[pos.y, pos.x]} zoom={11}>
         <TileLayer
           url="https://{s}.tile.osm.org/{z}/{x}/{y}.png"
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
@@ -101,7 +132,7 @@ export default () => {
               rectangle: false,
               circlemarker: false,
               circle: {
-                shapeOptions: { color: '#F00' },
+                shapeOptions: { color: '#FF0000AA' },
               },
             }}
           />
@@ -110,15 +141,77 @@ export default () => {
           <Popup>You are here.</Popup>
         </Marker>
         {hexs.map((p, i) => (
-          <Polygon key={i} color={selected[i] ? 'green' : '#af05f722'}
+          <Polygon key={i} color={zones.includes(p) ? '#048A0999' : '#AF05F722'}
             positions={p}
             onClick={() => {
-              console.log(p.index)
-              setSelection(sel => [...sel.slice(0, i), !sel[i], ...sel.slice(i + 1)])
+              setZones((zones) => {
+                const select = !zones.includes(p)
+                if(select) {
+                  return [p, ...zones]
+                } else {
+                  return zones.filter(z => z !== p)
+                }
+              })
             }}
           />
         ))}
       </LeafletMap>
+      <Table>
+        <thead><tr>
+          <th>Zone ID</th>
+          <th># of Open Contracts</th>
+          <th>🕊 to Center</th>
+        </tr></thead>
+        <tbody>
+          {zones.map((zone) => (
+            <tr key={`(${zone.index.row}, ${zone.index.col})`}>
+              <td>{`(${zone.index.row}, ${zone.index.col})`}</td>
+              <td>{zone.contracts.length}</td>
+              <td>{(() => {
+                // https://www.geeksforgeeks.org/program-distance-two-points-earth/
+                // d = 3963.0 * arccos[(sin(lat1) * sin(lat2)) + cos(lat1) * cos(lat2) * cos(long2 – long1)]
+                const rad = {
+                  1: { lat: pos.y / 180 / Math.PI, long: pos.x / 180 / Math.PI }, 
+                  2: { lat: zone.center.y / 180 / Math.PI, long: zone.center.x / 180 / Math.PI }, 
+                }
+                const dist = (
+                  3963.0 * Math.acos(
+                    Math.sin(rad[1].lat) * Math.sin(rad[2].lat)
+                    + Math.cos(rad[1].lat) * Math.cos(rad[2].lat)
+                    * Math.cos(rad[2].long - rad[1].long)
+                  )
+                )
+                return `${dist.toFixed(2)}mi`
+              })()}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+      <Table>
+        <thead><tr>
+          <th>🕊 to Pickup Waypoint</th>
+          <th>Waypoint to Sender</th>
+          <th>🕊 Pickup to Dropoff Waypoint</th>
+          <th>Waypoint to Recipient</th>
+          <th>Bid</th>
+        </tr></thead>
+        <tbody>
+          {contracts.map((con) => (
+            <tr>
+              <td>{con.pickup.waypoint}</td>
+              <td>{con.pickup.travelTime}</td>
+              <td>{con.dropoff.waypoint + con.pickup.waypoint}</td>
+              <td>{con.dropoff.travelTime}</td>
+              <td>
+                <Form onSubmit={alert}>
+                  <Input type='number' name='bid' />
+                  <Button>Bid</Button>
+                </Form>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
     </Flex>
   )
 }
