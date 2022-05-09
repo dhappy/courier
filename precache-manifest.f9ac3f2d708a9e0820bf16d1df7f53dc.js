e96e6fe0295dcfb14543007675ca91beb034edd1
W.J. “dysbulic” H <dys@dhappy.org>
2022-05-09 19:21:15 -0400

    Deploying `courier`

---
+++ precache-manifest.f9ac3f2d708a9e0820bf16d1df7f53dc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31f88c5fe233c85ae37fc1a74cf24203",
+    "url": "/index.html"
+  },
+  {
+    "revision": "30fd0310717a3d07d435",
+    "url": "/static/css/2.3f8b3d23.chunk.css"
+  },
+  {
+    "revision": "2a26186c93ec3663ff26",
+    "url": "/static/css/main.73c72131.chunk.css"
+  },
+  {
+    "revision": "30fd0310717a3d07d435",
+    "url": "/static/js/2.ec5b6432.chunk.js"
+  },
+  {
+    "revision": "3849b25ba222c2e36d739a2bb81a4734",
+    "url": "/static/js/2.ec5b6432.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a26186c93ec3663ff26",
+    "url": "/static/js/main.15abc370.chunk.js"
+  },
+  {
+    "revision": "e9648cb60be4d9680680",
+    "url": "/static/js/runtime-main.990ea932.js"
+  },
+  {
+    "revision": "fd5728f2cf777b06b966d05c0c823dc9",
+    "url": "/static/media/spritesheet.fd5728f2.svg"
+  }
+]);
