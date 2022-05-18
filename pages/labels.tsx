2df5124edc5bc6174c0e3fc772615421dfa228bb
W.J. “dysbulic” H <dys@dhappy.org>
2022-05-18 09:23:39 -0400

    migratin' labels to Next.js 🥏

--- pages/labels.tsx
+++ pages/labels.tsx
@@ -4,12 +4,16 @@ import {
   FormControl, FormLabel, Text, Select,
 } from '@chakra-ui/react'
 import { v1 as uuidv1 } from 'uuid'
-import * as base58 from 'bs58'
 import QRCode from 'qrcode'
-import { chunk } from '../utils'
+import { chunk, b64URL } from '../utils'
 import { create as mkIPFS } from 'ipfs-http-client';
 
-const links = async (path) => {
+type Datum = {
+  guid: string,
+  qr: string,
+}
+
+const links = async (path: string) => {
   // const url = 'https://dweb.link/api/v0'
   const url = 'http://localhost:5001/api/v0'
 
@@ -24,26 +28,35 @@ const links = async (path) => {
   return links
 }
 
+const genId = () => {
+  const buffer: Array<any> = []
+  uuidv1({}, buffer)
+  return b64URL(Buffer.from(buffer))
+}
+
 const Labels = () => {
   const [page, setPage] = useState({ width: 8.5, height: 11 })
   const [label, setLabel] = useState({ width: 2 + 5 / 8, height: 2 })
   const [innerGutter, setInnerGutter] = useState({ width: 1 / 8, height: 0 })
   const [outerGutter, setOuterGutter] = useState({ width: 7 / 32, height: 15 / 32 })
-  const [data, setData] = useState([])
-  const [images, setImages] = useState([])
+  const [data, setData] = useState<Array<Array<Datum>>>([])
+  const [images, setImages] = useState<Array<string>>([])
   const [cid, setCID] = useState(
     'QmT8fxdiii2hagjjTNf5xRwneW5KNopCNQaegLwuyVYZgu'
+  )
+  const [url, setURL] = useState(
+    'https://ship.via.autos/pkg/{guid}'
   )
 
   const dpi = 96 // inkscape default
   const presets = {
-    'Avery 6793 (15×(2“×2⅝“))': {
+    'Avery 6793 (15×(2”×2⅝”))': {
       page: { width: 8.5, height: 11 },
       label: { width: 2 + 5 / 8, height: 2 },
       outerGutter: { width: 7 / 32, height: 15 / 32 },
       innerGutter: { width: 1 / 8, height: 0 },
     },
-    'Avery 6874 (6×(3“×3¾“))': {
+    'Avery 6874 (6×(3”×3¾”))': {
       page: { width: 8.5, height: 11 },
       label: { width: 3 + 3 / 4, height: 3 },
       outerGutter: { width: 3 / 8, height: 5 / 8 },
@@ -55,8 +68,8 @@ const Labels = () => {
     innerGutter: setInnerGutter, outerGutter: setOuterGutter,
   }
 
-  const addImages = useCallback(async (cid) => {
-    const list = await links(cid)
+  const addImages = useCallback(async (cid: string) => {
+    const list = (await links(cid)).map((l) => l.path ?? l)
     console.debug('LOADING', list)
     setImages((imgs) => [...imgs, ...list])
   }, [])
@@ -70,15 +83,15 @@ const Labels = () => {
     )
     const qrs = await Promise.all(
       ids.map((guid) => (
-        new Promise((resolve, reject) => {
+        new Promise<string>((resolve, reject) => {
           QRCode.toString(
-            `https://ship.via.autos/pkg/${guid}`,
+            url.replace(/\{guid\}/g, guid),
             { margin: 0, errorCorrectionLevel: 'low' },
-            (err, string) => {
+            (err, str) => {
               if (err) {
-                reject(err)
+                reject(err.message)
               } else {
-                resolve(string)
+                resolve(str)
               }
             }
           )
@@ -86,39 +99,34 @@ const Labels = () => {
       ))
     )
     const data = ids.map((id, i) => ({ guid: id, qr: qrs[i] }))
-    setData(chunk(data, cols))
+    setData(chunk(data, cols) as Array<Array<Datum>>)
   }, [page, label, outerGutter, innerGutter])
 
   useEffect(() => { genData() }, [genData])
 
-  const setter = (dim, axis) => (
-    (evt) => {
-      const val = evt.target.value
-      if (!setters[dim]) {
+  const setter = (dim: string, axis: string) => (
+    ({ target: { value } }: { target: { value: string } }) => {
+      if (!setters[dim as keyof typeof setters]) {
         console.error(`No Setter For: ${dim}`)
       } else {
-        setters[dim](old => ({ ...old, [axis]: val }))
+        setters[dim as keyof typeof setters](
+          (old) => ({ ...old, [axis]: value })
+        )
       }
     }
   )
-
-  const genId = () => {
-    const buffer = []
-    uuidv1({}, buffer)
-    return base58.encode(buffer)
-  }
 
   const image = () => {
     if (images.length > 0) {
       const selected = images[Math.floor(images.length * Math.random())]
       return (
         // <image xlinkHref={`http://ipfs.io/ipfs/${selected.path}`} x="3%" y="-1.25%" height="6rem"/>
-        <image xlinkHref={`http://localhost:8888/ipfs/${selected.path}`} x="4%" y="-1.25%" height="6rem" />
+        <image xlinkHref={`http://localhost:8888/ipfs/${selected}`} x="4%" y="-1.25%" height="4rem" />
       )
     }
   }
 
-  const lbl = (datum, row, col) => {
+  const lbl = (datum: Datum, row: number, col: number) => {
     const x = (outerGutter.width + col * (label.width + innerGutter.width)) * dpi
     const y = (outerGutter.height + row * (label.height + innerGutter.height)) * dpi
     const initY = (outerGutter.height + label.height / 2) * dpi
@@ -138,69 +146,122 @@ const Labels = () => {
 
   return (
     <Stack align='center'>
-      <Box className='config'>
-        <Stack>
-          <Box as="form">
-            <FormControl>
-              <FormLabel>Page Size:</FormLabel>
-              <Input type="number" value={page.width} onChange={setter('page', 'width')} />
-              <Text as="span">×</Text>
-              <Input type="number" value={page.height} onChange={setter('page', 'height')} />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Label Size:</FormLabel>
-              <Input type="number" value={label.width} onChange={setter('label', 'width')} />
-              <Text as="span">×</Text>
-              <Input type="number" value={label.height} onChange={setter('label', 'height')} />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Outer Gutter Size:</FormLabel>
-              <Input type="number" value={outerGutter.width} onChange={setter('outerGutter', 'width')} />
-              <Text as="span">×</Text>
-              <Input type="number" value={outerGutter.height} onChange={setter('outerGutter', 'height')} />
-            </FormControl>
-            <FormControl>
-              <FormLabel>Inner Gutter Size:</FormLabel>
-              <Input type="number" value={innerGutter.width} onChange={setter('innerGutter', 'width')} />
-              <Text as="span">×</Text>
-              <Input type="number" value={innerGutter.height} onChange={setter('innerGutter', 'height')} />
-            </FormControl>
-          </Box>
+      <Stack
+        className='config'
+        sx={{ '@media print': { display: 'none' } }}
+      >
+        <Box as="form">
           <FormControl>
-            <FormLabel>Presets:</FormLabel>
-            <Select onChange={
-              ({ target: { value } }:{ target: { value: string } }) => {
-                const key = value as keyof typeof presets
-                const preset = presets[key]
-                for (let prop of Object.keys(setters)) {
-                  setters[prop as keyof typeof setters](
-                    preset[prop as keyof typeof preset]
-                  )
-                }
-              }
-            }>
-              {Object.keys(presets).map((opt, i) => (
-                <chakra.option key={i}>{opt}</chakra.option>
-              ))}
-            </Select>
+            <FormLabel>Page Size:</FormLabel>
+            <Input
+              type="number"
+              value={page.width}
+              onChange={setter('page', 'width')}
+            />
+            <Text as="span">” ×</Text>
+            <Input
+              type="number"
+              value={page.height}
+              onChange={setter('page', 'height')}
+            />
+            <Text as="span">”</Text>
           </FormControl>
           <FormControl>
-            <FormLabel>Add Images:</FormLabel>
+            <FormLabel>Label Size:</FormLabel>
             <Input
+              type="number"
+              value={label.width}
+              onChange={setter('label', 'width')}
+            />
+            <Text as="span">” ×</Text>
+            <Input
+              type="number"
+              value={label.height}
+              onChange={setter('label', 'height')}
+            />
+            <Text as="span">”</Text>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Outer Gutter Size:</FormLabel>
+            <Input
+              type="number"
+              value={outerGutter.width}
+              onChange={setter('outerGutter', 'width')}
+            />
+            <Text as="span">” ×</Text>
+            <Input
+              type="number"
+              value={outerGutter.height}
+              onChange={setter('outerGutter', 'height')}
+            />
+            <Text as="span">”</Text>
+          </FormControl>
+          <FormControl>
+            <FormLabel>Inner Gutter Size:</FormLabel>
+            <Input
+              type="number"
+              value={innerGutter.width}
+              onChange={setter('innerGutter', 'width')}
+            />
+            <Text as="span">” ×</Text>
+            <Input
+              type="number"
+              value={innerGutter.height}
+              onChange={setter('innerGutter', 'height')}
+            />
+            <Text as="span">”</Text>
+          </FormControl>
+        </Box>
+        <FormControl>
+          <FormLabel>Presets:</FormLabel>
+          <Select onChange={
+            ({ target: { value } }:{ target: { value: string } }) => {
+              const key = value as keyof typeof presets
+              const preset = presets[key]
+              for (let prop of Object.keys(setters)) {
+                setters[prop as keyof typeof setters](
+                  preset[prop as keyof typeof preset]
+                )
+              }
+            }
+          }>
+            {Object.keys(presets).map((opt, i) => (
+              <chakra.option key={i}>{opt}</chakra.option>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Add Images:</FormLabel>
+          <Flex>
+            <Input
+              flexGrow={1}
               value={cid}
               onChange={
                 ({ target: { value } }: { target: { value: string } }) => (
                   setCID(value)
                 )
               }
-              w="calc(100% - 6rem)"
             />
             <Button onClick={() => addImages(cid)}>
               <Text as="span" role="img">➕</Text>
             </Button>
-          </FormControl>
-        </Stack>
-      </Box>
+          </Flex>
+        </FormControl>
+        <FormControl>
+          <FormLabel>URL Template:</FormLabel>
+          <Flex>
+            <Input
+              flexGrow={1}
+              value={url}
+              onChange={
+                ({ target: { value } }: { target: { value: string } }) => (
+                  setURL(value)
+                )
+              }
+            />
+          </Flex>
+        </FormControl>
+      </Stack>
       <Flex>
         <svg
           viewBox={[0, 0, page.width * dpi, page.height * dpi].join(' ')}
